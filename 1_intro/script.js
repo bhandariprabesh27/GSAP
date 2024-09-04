210c4e60046d7af2bbd67663f4ea2cc0f6a6e0fe
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-04 22:25:20 +0545

    first commit

---
+++ 1_intro/script.js
@@ -0,0 +1,15 @@
+// gsap.to("#box", {
+//   x: 1000,
+//   rotate: 360,
+//   duration: 2,
+//   delay: 1,
+//   backgroundColor: "blue",
+// });
+
+gsap.from("#box", {
+  x: 1000,
+  rotate: 360,
+  duration: 2,
+  delay: 1,
+  backgroundColor: "blue",
+});
---
+++ 2_timeline/script.js
@@ -0,0 +1,25 @@
+var tl = gsap.timeline();
+
+tl.to("#box1", {
+  x: 1250,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2,
+  delay: 1,
+});
+
+tl.to("#box2", {
+  x: 1250,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2, //delay dinu parena
+});
+
+tl.to("#box3", {
+  x: 1250,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2, //delay dinu parena
+});
+
+//timeline ma delay dinu pardaina ra ek paxi arko chalxa
