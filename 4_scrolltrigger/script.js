3b8e22e827a7c2318cbcef44b261faccc34841d5
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-06 22:35:58 +0545

    final commit

---
+++ 4_scrolltrigger/script.js
@@ -0,0 +1,32 @@
+gsap.from("#page1 #circle", {
+  scale: 0,
+  duration: 2,
+  rotate: 720,
+  dealy: 1,
+});
+gsap.from("#page2 #circle", {
+  scale: 0,
+  duration: 2,
+  rotate: 720,
+  scrollTrigger: {
+    trigger: "#page2 #circle",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 2, //value of scrub must be either boolean(true/false) or (1-5)
+  },
+});
+gsap.from("#page3 #circle", {
+  scale: 0,
+  duration: 2,
+  rotate: 720,
+  scrollTrigger: {
+    trigger: "#page3 #circle",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 2, //value of scrub must be either boolean(true/false) or (1-5)
+  },
+});
